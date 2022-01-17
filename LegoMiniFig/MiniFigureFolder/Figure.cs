73d1da7b69f4f0f0315c8f094d68904bccbe063c
pmpurcell93 <maddenpurcell@gmail.com>
2022-01-16 20:45:35 -0600

    Add project files.

---
+++ LegoMiniFig/MiniFigureFolder/Figure.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegoMiniFig.MiniFigureFolder
+{
+    internal class Figure
+    {
+        // Constructor
+        public Figure(string name, string head, string torso, string legs, string accessories)
+        {
+            Name = name;    
+            Head = head;
+            Torso = torso;
+            Legs = legs;
+            Accessories = accessories;
+        }
+        //Hair or Hat, Head, Torso, Legs, Accessories
+        // Properties
+
+        public string Name { get; set; }
+        public string Head { get; set; }
+        public string Torso { get; set; }
+        public string Legs { get; set; }
+        public string Accessories { get; set; }
+
+        // Method
+
+        public void printDetails()
+        {
+            var output = ($@"
+Name: {Name}
+Head: {Head}
+Torso: {Torso}
+Legs: {Legs}
+Accessories: {Accessories}");
+
+            Console.WriteLine($"{output}");
+        }
+    }
+}
---
+++ LegoMiniFig/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using LegoMiniFig.MiniFigureFolder;
+
+namespace LegoMiniFig
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Spaceman = new Figure("Spaceman","Space Helmet", "Spacesuit", "Space Pants", "Jetpack");
+
+            Spaceman.printDetails();
+
+            var Ninja = new Figure("Ninja", "Headband", "Jacket", "Trousers", "Katana");
+
+            Ninja.printDetails();
+        }
+    }
+}
